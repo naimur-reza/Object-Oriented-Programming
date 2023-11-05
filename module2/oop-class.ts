9a88242dbec856748e4c78e17fc2f59e091c9972
Naimur Reza <naimur.rezaa@gmail.com>
2023-11-06 00:03:48 +0600

    Params makes life easier

--- module2/oop-class.ts
+++ module2/oop-class.ts
@@ -2,14 +2,15 @@
   //  oop - class
 
   class Person {
-    public name: string;
-    public age: number;
-    public email: string;
+    // public name: string;
+    // public age: number;
+    // public email: string;
 
-    constructor(name: string, age: number, email: string) {
-      this.name = name;
-      this.age = age;
-      this.email = email;
+    //   parameter properties
+    constructor(public name: string, public age: number, public email: string) {
+      //   this.name = name;
+      //   this.age = age;
+      //   this.email = email;
     }
   }
 
