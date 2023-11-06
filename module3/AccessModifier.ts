08f47fae76b227b76c79400c4da51f17ad785a22
Naimur Reza <naimur.rezaa@gmail.com>
2023-11-06 23:38:53 +0600

    Private & Public with Method

--- module3/AccessModifier.ts
+++ module3/AccessModifier.ts
@@ -1,4 +1,23 @@
 {
   //
+
+  class BankAccount {
+    public id: number;
+    public name: string;
+    private balance: number;
+
+    constructor(id: number, name: string, balance: number) {
+      this.balance = balance;
+      this.name = name;
+      this.id = id;
+    }
+
+    addDeposit(balance: number) {
+      return this.balance + balance;
+    }
+  }
+
+  const johnAccount = new BankAccount(123, "John", 29);
+  console.log(johnAccount.addDeposit(44));
   //
 }
