b6de5482a6ed2c5a92a3d9476463ea21e8757e62
Naimur Reza <naimur.rezaa@gmail.com>
2023-11-07 00:10:55 +0600

    Getter and Setup add

--- module3/Getter&Setter.ts
+++ module3/Getter&Setter.ts
@@ -17,12 +17,24 @@
       this._owner = owner;
     }
 
+    // getter and setter method
+
+    // use getter for get the owner
     get owner() {
       return this._owner;
+    }
+
+    // use setter to update the owner
+
+    set updateOwner(name: string) {
+      this._owner = name;
     }
   }
 
   const newCar = new Car("Mazda", "NITRO-1500", "2013", "Laura");
+
+  newCar.updateOwner = "Ray";
+
   const owner = newCar.owner;
   console.log(owner);
 }
