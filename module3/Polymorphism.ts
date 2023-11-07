648a6cbcfa4aa4588189f11efd3d08345f21bdc3
Naimur Reza <naimur.rezaa@gmail.com>
2023-11-07 18:29:00 +0600

    Polymorphism

---
+++ module3/Polymorphism.ts
@@ -0,0 +1,25 @@
+{
+  // polymorphism
+
+  class Person {
+    public getSleep(): void {
+      console.log("I'm a normal kiddo, i sleep 8 hr per day");
+    }
+  }
+
+  class Developer extends Person {
+    public getSleep(): void {
+      console.log("I'm a developer. I sleep 6hr per day!");
+    }
+  }
+
+  const getSleepingHours = (param: Person) => {
+    return param.getSleep();
+  };
+
+  const person = new Person();
+  const developer = new Developer();
+
+  getSleepingHours(person);
+  getSleepingHours(developer);
+}
